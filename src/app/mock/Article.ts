b9abafb5eb336b725dfc4eb13910ea3272be3716
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-04-18 20:56:18 +0200

    Mise en place des légendes photos / Article principal / Autres destinations

--- src/app/mock/Article.ts
+++ src/app/mock/Article.ts
@@ -39,12 +39,12 @@ export const ARTICLES: Iarticle[] = [
   },
   {
     id: 2,
-    country: 'USA',
-    city: 'New-York',
+    country: 'Grèce',
+    city: 'Athène',
     departureDate: '01/01/2023',
     returnDate: '16/01/2023',
     mainImage: '../assets/mainImages/Albanie.png',
-    secondaryImages: '/assets/secondaryImages/AlbanieSecondary.png',
+    secondaryImages: '/assets/secondaryImages/grece.png',
     gallery: [],
     tripDescription:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget velit aliquet sagittis id consectetur purus ut. Sed risus pretium quam vulputate dignissim suspendisse in est. Lectus sit amet est placerat in egestas erat. Morbi tincidunt ornare massa eget egestas. Purus faucibus ornare suspendisse sed. Montes nascetur ridiculus mus mauris vitae. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Turpis in eu mi bibendum. Nunc vel risus commodo viverra maecenas accumsan. Pulvinar proin gravida hendrerit lectus a.',
@@ -60,12 +60,12 @@ export const ARTICLES: Iarticle[] = [
   },
   {
     id: 3,
-    country: 'United-Kingdom',
-    city: 'London',
+    country: 'Guatemala',
+    city: 'Monterrico',
     departureDate: '14/09/2022',
     returnDate: '02/10/2022',
     mainImage: '../assets/mainImages/Albanie.png',
-    secondaryImages: '/assets/secondaryImages/AlbanieSecondary.png',
+    secondaryImages: '/assets/secondaryImages/guatemala.png',
     gallery: [],
     tripDescription:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget velit aliquet sagittis id consectetur purus ut. Sed risus pretium quam vulputate dignissim suspendisse in est. Lectus sit amet est placerat in egestas erat. Morbi tincidunt ornare massa eget egestas. Purus faucibus ornare suspendisse sed. Montes nascetur ridiculus mus mauris vitae. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Turpis in eu mi bibendum. Nunc vel risus commodo viverra maecenas accumsan. Pulvinar proin gravida hendrerit lectus a.',
@@ -81,12 +81,12 @@ export const ARTICLES: Iarticle[] = [
   },
   {
     id: 4,
-    country: 'Italy',
-    city: 'Napoli',
+    country: 'Tunisie',
+    city: 'Tunis',
     departureDate: '13/06/2022',
     returnDate: '31/06/2022',
     mainImage: '../assets/mainImages/Albanie.png',
-    secondaryImages: '/assets/secondaryImages/AlbanieSecondary.png',
+    secondaryImages: '/assets/secondaryImages/tunisie.png',
     gallery: [],
     tripDescription:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget velit aliquet sagittis id consectetur purus ut. Sed risus pretium quam vulputate dignissim suspendisse in est. Lectus sit amet est placerat in egestas erat. Morbi tincidunt ornare massa eget egestas. Purus faucibus ornare suspendisse sed. Montes nascetur ridiculus mus mauris vitae. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Turpis in eu mi bibendum. Nunc vel risus commodo viverra maecenas accumsan. Pulvinar proin gravida hendrerit lectus a.',
